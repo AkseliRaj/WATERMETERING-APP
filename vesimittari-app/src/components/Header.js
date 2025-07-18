c7761e03a0517b1c6e8e849c9e288c723b03f2d8
AkseliRaj <akseli.rajahalme@gmail.com>
2025-07-18 18:12:53 +0300

    Made more form changes

--- vesimittari-app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-      <header className="mb-4">
-        <h1>Vedenkäyttöilmoitus</h1>
-      </header>
-    );
-  }
-  
-  export default Header;
--- vesimittari-app/src/components/VesimittariForm.js
+++ vesimittari-app/src/components/VesimittariForm.js
@@ -2,29 +2,45 @@ import React, { useState } from 'react';
 
 function VesimittariForm() {
     const [nimi, setNimi] = useState('');
-    const [lukema, setLukema] = useState('');
+    const [paivamaara, setPaivamaara] = useState('');
+    const [vesilukema, setVesilukema] = useState('');
+    const [lisatietoja, setLisatietoja] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Nimi: ${nimi}, Lukema: ${lukema}`);
+        alert(`Nimi: ${nimi}, Päivämäärä: ${paivamaara}, Vesilukema: ${vesilukema}, Lisätietoja: ${lisatietoja}`);
         // Tähän kohtaan myöhemmin tallennus backend-tietokantaan
     };
 
     return (
-        <div className="container">
-            <form onSubmit={handleSubmit} className="p-5">
-                <div className="mb-5">
+        <div className="container p-4">
+            <h2 className="mb-3 mt-5">Vedenkäyttöilmoitus</h2>
+            <form onSubmit={handleSubmit} className="col-10 pt-4">
+                <div className="mb-4">
                     <label className="form-label">Nimi</label>
                     <input className="form-control" value={nimi} onChange={e => setNimi(e.target.value)} required />
                 </div>
-                <div className="mb-5">
-                    <label className="form-label">Vesimittarin lukema</label>
-                    <input className="form-control" type="number" value={lukema} onChange={e => setLukema(e.target.value)} required />
+                <div className="mb-4">
+                    <label className="form-label">Lukeman päivämäärä</label>
+                    <input className="form-control" type="date" value={paivamaara} onChange={e => setPaivamaara(e.target.value)} required />
+                </div>
+                <div className="mb-4">
+                    <label className="form-label">Vesilukema</label>
+                    <input className="form-control" type="number" value={vesilukema} onChange={e => setVesilukema(e.target.value)} required />
+                </div>
+                <div className="mb-4">
+                    <label className="form-label">Lisätietoja</label>
+                    <textarea 
+                        className="form-control" 
+                        rows="3" 
+                        value={lisatietoja} 
+                        onChange={e => setLisatietoja(e.target.value)}
+                        placeholder="Voit esittää esimerkiksi kulutuspoikkeaman syyn."
+                    ></textarea>
                 </div>
                 <button className="btn btn-primary" type="submit">Lähetä</button>
             </form>
         </div>
-
     );
 }
 
